38a2a49fadf8f196c59e02ef3d67b433a32900d8
juvajuva <593354129@qq.com>
2017-11-10 13:32:13 +0800

    fix(history):目录大写

---
+++ template/log/History/tips.php
@@ -0,0 +1,9 @@
+<?php include(__DIR__ . '/../common/header.php') ?>
+<?php include(__DIR__ . '/../common/nav.php') ?>
+<div class="container-fluid theme-showcase" role="main">
+    <div class="page-header">
+    请配置host:&nbsp;&nbsp;192.168.0.104 historylog.meilele.com 后点击&nbsp;
+        <a href="http://historylog.meilele.com/log/History/just" target="_blank">GO</a>
+    </div>
+</div> <!-- /container -->
+<?php include(__DIR__ . '/../common/footer.php') ?>
